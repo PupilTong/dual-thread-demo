2400f40a74017d63a29493a25205c29800fdf42b
pupiltong <12288479+PupilTong@users.noreply.github.com>
2025-08-31 23:59:01 +0800

    add lynx native dev settings

--- packages/lynx-heic-runtime-decode/lynx.config.ts
+++ packages/lynx-heic-runtime-decode/lynx.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@lynx-js/rspeedy';
 import { pluginNodePolyfill } from '@rsbuild/plugin-node-polyfill';
 import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin';
+import { pluginQRCode } from '@lynx-js/qrcode-rsbuild-plugin'
 export default defineConfig({
   plugins: [pluginReactLynx(), pluginNodePolyfill()],
   source: {
@@ -13,5 +14,10 @@ export default defineConfig({
         assetPrefix: 'auto',
       },
     },
+    lynx:{
+      plugins: [
+        pluginQRCode(),
+      ]
+    }
   },
 });
